23f18d3a208a1308cce34ba6a729a9a7e58cb7ae
Sunny <sunnysagar620@gmail.com>
2022-06-25 20:35:37 +0530

    commit the changes

---
+++ app/src/main/java/com/sunnysagar/musicworld/PlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.sunnysagar.musicworld
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
